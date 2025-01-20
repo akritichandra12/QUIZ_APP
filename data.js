f06044e1f88580256caf7550e8b053595fdd6662
Akriti Chandra <akritichandra120803@gmail.com>
2025-01-20 17:21:13 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,43 @@
+export const data=[
+    {
+        question:"Which device is required for internet connection?",
+        option1:"Modem",
+        option2:"Router",
+        option3:"LAN Cable",
+        option4:"Pen Drive",
+        ans: 1,
+    },
+    {
+        question:"Which continent has the highest number of countries?",
+        option1:"Asia",
+        option2:"Europe",
+        option3:"North America",
+        option4:"Africa",
+        ans: 4,
+    },
+    {
+        question:"A computer cannot BOOT if it does not have the?",
+        option1:"Application Software",
+        option2:"Mouse",
+        option3:"Operating System",
+        option4:"Internet",
+        ans: 3,
+    },
+    {
+        question:"First page of website is called?",
+        option1:"Index Page",
+        option2:"Home Page",
+        option3:"Sitemap",
+        option4:"Pen Drive",
+        ans: 2,
+    },
+    {
+        question:"Junk e-mail is also called?",
+        option1:"Spam",
+        option2:"Fake",
+        option3:"Archived",
+        option4:"Bin",
+        ans: 1,
+    },
+
+];
